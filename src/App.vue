1b582e502b9870d9e2e5c4524ab5be1df2f3030d
alixb <null@none.com>
2022-08-31 17:23:22 +0200

    Feature: Display the new ToDoListView in the app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDoListView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoListView from "./views/ToDoListView";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoListView
   }
 }
 </script>
---
+++ src/views/ToDoListView.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        components: {
+            
+        }
+    }
+</script>
+
+<template>
+  <h1>To Do List</h1>
+
+  <div class="to-do-list-panel">
+    <div class="buttons-panel">
+      <button>-</button>
+      <button>+</button>
+    </div>
+
+    <div class="taks-panel">
+      <p>Task 1</p>
+      <p>Task 2</p>
+      <p>Task 3</p>
+    </div>
+  </div>
+  
+</template>
